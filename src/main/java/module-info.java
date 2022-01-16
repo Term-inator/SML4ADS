7afe4bff01dc5851c536d87eb063753a51f4797b
Term-inator <1121745627@qq.com>
2022-01-16 21:56:46 +0800

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.openfx.adsmls {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.ecnu.adsmls to javafx.fxml;
+    exports com.ecnu.adsmls;
+}
