1ad55b862b7fa2328ab3d45873d469224678a719
Sicong Chen <61724252+Term-inator@users.noreply.github.com>
2022-01-16 22:21:41 +0800

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.openfx.adsmls {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.ecnu.adsmls to javafx.fxml;
-    exports com.ecnu.adsmls;
-}
