249595f7961bd92ecfb83465879c95f81ded2625
Term-inator <1121745627@qq.com>
2022-02-13 01:05:29 +0800

    create project

---
+++ src/main/java/com/ecnu/adsmls/utils/FileSystem.java
@@ -0,0 +1,41 @@
+package com.ecnu.adsmls.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileSystem {
+    public static boolean createFile(String directory, String filename) {
+        String pathName = directory + filename;
+        File file = new File(pathName);
+        if(!file.getParentFile().exists()) {
+            if(!file.getParentFile().mkdirs()) {
+                return false;
+            }
+        }
+        try {
+            if(file.createNewFile()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean createDir(String directory) {
+        File dir = new File(directory);
+        if(!directory.endsWith(File.separator)) {
+            directory = directory + File.separator;
+        }
+        if(dir.mkdirs()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
