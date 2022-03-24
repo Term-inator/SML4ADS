6bb9620a18d1aa7fe4fccf87f5d760db78f64c3b
Term-inator <1121745627@qq.com>
2022-03-24 21:45:44 +0800

    refactor: SettingsModal 会显示已有的配置信息

--- src/main/java/com/ecnu/adsmls/components/modal/SettingsModal.java
+++ src/main/java/com/ecnu/adsmls/components/modal/SettingsModal.java
@@ -10,17 +10,26 @@ import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 // TODO 暂时使用 Modal
 public class SettingsModal extends Modal {
+    private MConfig mConfig;
     private String pythonInterpreter;
 
     private Node btPythonInterpreter;
 
-    public SettingsModal() {
+    public SettingsModal(MConfig mConfig) {
         super();
+        this.mConfig = mConfig;
+        this.loadData();
+    }
+
+    private void loadData() {
+        this.pythonInterpreter = this.mConfig.getPythonEnv();
     }
 
     protected void createWindow() {
@@ -32,6 +41,9 @@ public class SettingsModal extends Modal {
         Map<String, String> exeFilter = new HashMap<>();
         exeFilter.put(FileSystem.getRegSuffix(FileSystem.Suffix.EXE), FileSystem.Suffix.EXE.toString());
         this.btPythonInterpreter = new ChooseFileButton(this.gridPane, exeFilter).getNode();
+        if(!Objects.equals(this.pythonInterpreter, "")) {
+            ((ChooseFileButton) this.btPythonInterpreter.getUserData()).setFile(new File(this.pythonInterpreter));
+        }
 
         this.slot.addRow(0, lbPythonInterpreter, this.btPythonInterpreter);
     }
--- src/main/java/com/ecnu/adsmls/views/codepage/CodePageController.java
+++ src/main/java/com/ecnu/adsmls/views/codepage/CodePageController.java
@@ -39,6 +39,9 @@ public class CodePageController implements Initializable, Route {
     // 目录的上下文菜单
     private List<MenuItem> multiLevelDirectoryMenu = new ArrayList<>();
 
+    // 项目配置
+    private MConfig mConfig;
+
     @FXML
     private TabPane tabPane;
     // 正在被访问的文件
@@ -89,14 +92,13 @@ public class CodePageController implements Initializable, Route {
         // 设置
         MenuItem setting = new MenuItem("Settings");
         setting.setOnAction(e -> {
-            SettingsModal sm = new SettingsModal();
+            SettingsModal sm = new SettingsModal(this.mConfig);
             sm.getWindow().showAndWait();
             if(!sm.isConfirm()) {
                 return;
             }
             // 写入配置文件
-            MConfig mConfig = new MConfig();
-            mConfig.setPythonEnv(Global.pythonEnv);
+            this.mConfig.setPythonEnv(Global.pythonEnv);
 
             String config = JSON.toJSONString(mConfig);
             System.out.println(config);
@@ -171,12 +173,12 @@ public class CodePageController implements Initializable, Route {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            MConfig mConfig = JSON.parseObject(config, MConfig.class);
-            if(mConfig == null) {
+            this.mConfig = JSON.parseObject(config, MConfig.class);
+            if(this.mConfig == null) {
                 return;
             }
 
-            Global.pythonEnv = mConfig.getPythonEnv();
+            Global.pythonEnv = this.mConfig.getPythonEnv();
         }
     }
 
